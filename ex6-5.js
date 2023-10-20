ceca90bee7a81b5274fff9c766c4758a9c20ef61
zm21202yuta <131091894+zm21202yuta@users.noreply.github.com>
2023-10-20 09:46:30 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -3,9 +3,4 @@ function sayhello65(){ alert('Hello,'+who65+'!');}
 function taro65(){who65='太郎';}
 function hanako65(){who65='花子';}
 
-var who66='world';
-function  sayhello66(){ alert('Hello,'+who66+'!');}
-function taro66(){who66='太郎';}
-function hanako66(){who66='花子';}
 
-function saycome66(){ alert(who66+'が来た!');}
