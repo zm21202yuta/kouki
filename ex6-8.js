baab887e9a81738a02d264e2fd433b1b52ea0f79
zm21202yuta <131091894+zm21202yuta@users.noreply.github.com>
2023-10-24 10:32:53 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -2,4 +2,6 @@ function sayhello68(){ alert('Hello,'+ document.getElementById('who68').innerHTM
 function taro68(){ document.getElementById('who68').innerHTML='太郎';}
 function hanako68(){ document.getElementById('who68').innerHTML='花子';}
 
-function gazo688(){ document.getElementById('who688').innerHTML='<img src="./image/riza-donn.jpg" alt="オン">';}
+function gazo688(){ document.getElementById('who688').innerHTML=
+  '<img src="./image/riza-donn.jpg" alt="オン">';}
+
