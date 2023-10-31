ec4e15ae4ad91e5a62db0f0c62731bcbb89cf56f
zm21202yuta <131091894+zm21202yuta@users.noreply.github.com>
2023-10-31 10:49:39 +0900

    Update ex6-8.js

--- ex6-8.js
+++ ex6-8.js
@@ -3,5 +3,5 @@ function taro68(){ document.getElementById('who68').innerHTML='太郎';}
 function hanako68(){ document.getElementById('who68').innerHTML='花子';}
 function gazo688(){ document.getElementById('who688').innerHTML='<img src="./image/riza-donn.jpg" alt="オン">';}
 
-function 
+function imgchange2(){documentByID('who688').innerHTML='<img src="./image/">'} 
 
