6ea8083ac20ad8e8a1d8103442897cd8f887dc3b
zm21202yuta <131091894+zm21202yuta@users.noreply.github.com>
2023-11-07 09:13:29 +0900

    Update ex6-10-2.js

--- ex6-10-2.js
+++ ex6-10-2.js
@@ -1,5 +1,3 @@
 function imgchange3(){ document.getElementById('poke').src = './image/riza-donn.jpg';}
 
 function imgchange4(){ document.getElementById('poke').src = './image/hitokage.jpg';}
-hitokage.jpg
-riza-donn.jpg
