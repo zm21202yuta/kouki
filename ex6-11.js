fd4b4859dac3bf7e22d22fd25100a18ed569a5dc
zm21202yuta <131091894+zm21202yuta@users.noreply.github.com>
2023-11-07 10:08:09 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,10 +1,15 @@
-function hyoji(){//表示する文字 
+function hyoji(){
+//表示する文字 
 var str="一文字ずつ表示します。";
-  //テキストボックスの文字数
+//テキストボックスの文字数
 var cnt= document.timer.moji.value.length;
-//文字が全部表示されているか確認}
-if(cnt<11){//現在より１文字多く切り出して表示 document.timer.moji.value=str.substr(0,cnt+1);}
-  else{//すべて表示されたら、空文字に戻す document.timer.moji.value="";}}
+//文字が全部表示されているか確認
+if(cnt<11){
+//現在より１文字多く切り出して表示
+document.timer.moji.value=str.substr(0,cnt+1);}
+else{
+//すべて表示されたら、空文字に戻す 
+document.timer.moji.value="";}}
   function startfnc(){//関数hyouji()を1000ミリ秒間隔で呼び出す
   setinterval("hyouji()",1000);}
   
