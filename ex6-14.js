b28280606e68e578ec40baf8a5caee48852e2339
zm21202yuta <131091894+zm21202yuta@users.noreply.github.com>
2023-11-14 10:23:04 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,1 +1,17 @@
+var image=new Array();
+image[0]=new Image();
+image[0].src="img/hitokage.jpg";
+image[1]=new Image();
+image[1].src="img/riza-donn.jpg";
+var cnt=0;
+function henkou14(){
+if(document.getElementById){
+  document.henkou.elements[0].disabled=true;
+  document.getElementById("gh").src=image[cnt].src;
+  cnt++;
+  if(cnt<=1)
+    var timer1=setTimeout("henkou14()",3000);}else{
+  cnt=0;
+  document.henkou.elements[0].disabled=false;
+  clearTimeout(timer1);}}}
 
