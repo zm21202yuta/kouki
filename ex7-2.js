960062db2731a39f1c26d4c562b13d424252afe5
zm21202yuta <131091894+zm21202yuta@users.noreply.github.com>
2023-11-30 14:12:48 +0900

    Update ex7-2.js

--- ex7-2.js
+++ ex7-2.js
@@ -1,5 +1,5 @@
 function add(){
-  var x= document.getElementById('input1').value;
-  var y= document.getElementById('input2').value;
+  var x= Number(document.getElementById('input1').value);
+  var y= Number(document.getElementById('input2').value);
   alert(x+y);
 }
