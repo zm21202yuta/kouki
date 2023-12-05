d63df68a1736785c91bd75714550bd0c009151f0
zm21202yuta <131091894+zm21202yuta@users.noreply.github.com>
2023-12-05 10:19:08 +0900

    Update ex7-8.js

--- ex7-8.js
+++ ex7-8.js
@@ -1,5 +1,5 @@
 function nazo772() {
-  if (document.getElementById('answer772').value == '夢') {
+  if (document.getElementById('answer772').value = '夢') {
     alert('あたり!');
   } else {
     alert('はずれ!');
