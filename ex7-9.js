a73ba9b52d2c9d9b3b53c4a34c3ff8de5ba290eb
zm21202yuta <131091894+zm21202yuta@users.noreply.github.com>
2023-12-05 10:31:27 +0900

    Create ex7-9.js

---
+++ ex7-9.js
@@ -0,0 +1,9 @@
+function nazo773() {
+  if (document.getElementById('answer773').value == '夢') {
+    alert('あたり!');
+  } else if (document.getElementById('answer773').value == 'ゆめ') {
+    alert('あたり!');
+  } else {    
+    alert('はずれ!');
+  }
+}
