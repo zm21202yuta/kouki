483e6548c3e444a27b43754cddd6680594d328eb
zm21202yuta <131091894+zm21202yuta@users.noreply.github.com>
2023-12-05 09:12:23 +0900

    Update ex7-3.js

--- ex7-3.js
+++ ex7-3.js
@@ -1,1 +1,8 @@
+function BMI() {
+    var height = Number(document.getElementById('height').value) / 100; 
+    var weight = Number(document.getElementById('weight').value);
+    var bmi = weight / (height * height);
+
+    alert('あなたのBMIは ' + bmi + ' です。');
+}
 
